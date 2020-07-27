e1f5bf9c058e7456cc311148314338c99f063e05
Yang Lu <yluu@google.com>
2020-07-27 07:03:48 -0700

    Modifications for trigger from pubsub notifications

--- trigger_build_from_gcr_pubsub.sh
+++ trigger_build_from_gcr_pubsub.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+function build_from_versions {
+    versions_array=$1
+    echo "versions_array: ${versions_array[@]}"
+    substitutions=""
+    for ((i = 0; i < ${#versions_array[@]}; i++)); do
+        ver=${versions_array[$i]}
+        substitutions+="_VERSION$((i+1))=${versions_array[$i]}," 
+        image=$(gcloud compute images list --filter="name:(${ver/ltsc/}-dc-core-for-containers)" --project=windows-cloud --format="value(name)")
+        substitutions+="_IMAGE$((i+1))=windows-cloud/global/images/$image,"
+    done
+    substitutions+="_OUTPUT_IMAGE_NAME=multiarch-demo"
+    echo "substitutions: $substitutions"
+    gcloud builds submit --config=cloudbuild.yaml --substitutions=$substitutions
+}
+
 image=$(gcloud pubsub subscriptions pull windows_container_update --auto-ack --format='value(DATA)' | jq -r '.tag | split(":")[0]')
 if [ -n "$image" ]
 then
@@ -10,6 +25,5 @@ then
         versions_array+=(`echo $tag | tr ',' ' '`)
     done
     echo "versions_array: ${versions_array[@]}"
-    source ./build.sh
     build_from_versions $versions_array
 fi
--- trigger_build_from_tagListFile_update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source ./build.sh
-mapfile -t versions_array < tagListFile.txt
-build_from_versions $versions_array
