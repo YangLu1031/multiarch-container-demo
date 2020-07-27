9d096fd57cf8bd6258ff39149174f71e599a9edf
Yang Lu <yluu@google.com>
2020-07-27 06:55:20 -0700

    Modifications for trigger from github only

--- trigger_build_from_tagListFile_update.sh
+++ trigger_build_from_tagListFile_update.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-source ./build.sh
-mapfile -t versions_array < tagListFile.txt
+function build_from_versions {
+    versions_array=$1
+    echo "versions_array: ${versions_array[@]}"
+    substitutions=""
+    # Lookup the GCE images from the version tags.
+    for ((i = 0; i < ${#versions_array[@]}; i++)); do
+        ver=${versions_array[$i]}
+        substitutions+="_VERSION$((i+1))=${versions_array[$i]}," 
+        image=$(gcloud compute images list --filter="name:(${ver/ltsc/}-dc-core-for-containers)" --project=windows-cloud --format="value(name)")
+        substitutions+="_IMAGE$((i+1))=windows-cloud/global/images/$image,"
+    done
+    substitutions+="_OUTPUT_IMAGE_NAME=multiarch-demo"
+    echo "substitutions: $substitutions"
+    # Trigger cloud build
+    gcloud builds submit --config=multiarchBuild.yaml --substitutions=$substitutions
+}
+mapfile -t versions_array < tagList.txt
 build_from_versions $versions_array
