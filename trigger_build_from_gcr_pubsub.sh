9c4040fcc12b661d0792c4d3139c0c98891a834d
Yang Lu <yluu@google.com>
2020-08-02 06:37:48 -0700

    Update to use gke-windows-builder

--- trigger_build_from_gcr_pubsub.sh
+++ trigger_build_from_gcr_pubsub.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
-function build_from_versions {
-    versions_array=$1
-    echo "versions_array: ${versions_array[@]}"
-    substitutions=""
-    for ((i = 0; i < ${#versions_array[@]}; i++)); do
-        ver=${versions_array[$i]}
-        substitutions+="_VERSION$((i+1))=${versions_array[$i]}," 
-        image=$(gcloud compute images list --filter="name:(${ver/ltsc/}-dc-core-for-containers)" --project=windows-cloud --format="value(name)")
-        substitutions+="_IMAGE$((i+1))=windows-cloud/global/images/$image,"
-    done
-    substitutions+="_OUTPUT_IMAGE_NAME=multiarch-demo"
-    echo "substitutions: $substitutions"
-    gcloud builds submit --config=cloudbuild.yaml --substitutions=$substitutions
-}
-
-image=$(gcloud pubsub subscriptions pull windows_container_update --auto-ack --format='value(DATA)' | jq -r '.tag | split(":")[0]')
-if [ -n "$image" ]
-then
+while true; do
+    image=$(gcloud pubsub subscriptions pull windows_container_update --auto-ack --format='value(DATA)' | jq -r '.tag | split(":")[0]')
     echo "image: $image"
-    tag_list=$(gcloud container images list-tags $image --format='value(TAGS)' --filter='tags:*') 
-    echo "tag_list: ${tag_list[@]}"
-    declare -a versions_array
-    for tag in ${tag_list[@]}; do
-        versions_array+=(`echo $tag | tr ',' ' '`)
-    done
-    echo "versions_array: ${versions_array[@]}"
-    build_from_versions $versions_array
-fi
+    if [[ $image =~ "gke-windows-builder" ]]
+    then
+        echo "found the image update: $image"
+        gcloud builds submit --config=cloudbuild.yaml .
+    fi
+    sleep 5
+done
